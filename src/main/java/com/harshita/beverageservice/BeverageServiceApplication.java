cea0c5aa5fd9b5182d12f48b219f005c801b7a08
harshita <harshitapant96@gmail.com>
2023-05-18 12:28:32 +0530

    Basic Project Structure

---
+++ src/main/java/com/harshita/beverageservice/BeverageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.harshita.beverageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeverageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeverageServiceApplication.class, args);
+	}
+
+}
