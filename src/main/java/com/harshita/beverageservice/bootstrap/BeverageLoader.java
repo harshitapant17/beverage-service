c7cc27d41255cd10729f4031e8f76e9a8166eb3d
harshita <harshitapant96@gmail.com>
2023-05-26 16:57:06 +0530

    Adding Bootstrap class

---
+++ src/main/java/com/harshita/beverageservice/bootstrap/BeverageLoader.java
@@ -0,0 +1,49 @@
+package com.harshita.beverageservice.bootstrap;
+
+import com.harshita.beverageservice.domain.Beverage;
+import com.harshita.beverageservice.repositories.BeverageRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeverageLoader implements CommandLineRunner {
+
+    private final BeverageRepository beverageRepository;
+
+    public BeverageLoader(BeverageRepository beverageRepository) {
+        this.beverageRepository = beverageRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeverageObjects();
+    }
+
+    private void loadBeverageObjects() {
+
+        if(beverageRepository.count()==0)
+        {
+            beverageRepository.save(Beverage.builder()
+                    .beverageName("Virgin Mojito")
+                    .beverageStyle("Regular")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(3370100000001L)
+                    .price(new BigDecimal("250"))
+                    .build());
+
+            beverageRepository.save(Beverage.builder()
+                    .beverageName("Melon Smash")
+                    .beverageStyle("Regular")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(3370100000002L)
+                    .price(new BigDecimal("310"))
+                    .build());
+        }
+
+        //System.out.println("Loaded Beverages: "+beverageRepository.count());
+    }
+}
--- src/main/java/com/harshita/beverageservice/repositories/BeverageRepository.java
+++ src/main/java/com/harshita/beverageservice/repositories/BeverageRepository.java
@@ -1,10 +1,11 @@
 package com.harshita.beverageservice.repositories;
 
 import com.harshita.beverageservice.domain.Beverage;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface BeverageRepository extends PagingAndSortingRepository<Beverage, UUID> {
+public interface BeverageRepository extends JpaRepository<Beverage, UUID> {
 
 }
